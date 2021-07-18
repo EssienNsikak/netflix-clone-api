2667f4c8f5e86408e3c9dbd6c53df8372b331a37
EssienNsikak <nsikak.essienceo@gmail.com>
2021-07-18 21:20:17 +0100

    minor fixes on user route

--- routes/users.js
+++ routes/users.js
@@ -75,7 +75,7 @@ router.get('/', verify, async (req, res) => {
 //GET USER STATS
 router.get('/stats', async (req, res) => {
   const today = new Date();
-  const latYear = today.setFullYear(today.setFullYear() - 1);
+  const lastYear = today.setFullYear(today.setFullYear() - 1);
 
   try {
     const data = await User.aggregate([

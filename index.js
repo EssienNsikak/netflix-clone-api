8ebb549d055856c981fed0a04a73051a685fce86
EssienNsikak <nsikak.essienceo@gmail.com>
2021-07-18 19:13:39 +0100

    mongoDB connection modified

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useFindAndModify: false
   })
   .then(() => console.log('DB Connection Successful'))
   .catch((err) => {

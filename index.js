32f59e0d8ea4b0c051b171cec5bd6ba472ab6ab3
EssienNsikak <nsikak.essienceo@gmail.com>
2021-07-18 15:43:12 +0100

    routes added and index.js modified

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-// const authRoute = require('./routes/auth');
-// const userRoute = require('./routes/users');
-// const movieRoute = require('./routes/movies');
-// const listRoute = require('./routes/lists');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const movieRoute = require('./routes/movies');
+const listRoute = require('./routes/lists');
 
 dotenv.config();
 
@@ -23,10 +23,10 @@ mongoose
 
 app.use(express.json());
 
-// app.use('/api/auth', authRoute);
-// app.use('/api/users', userRoute);
-// app.use('/api/movies', movieRoute);
-// app.use('/api/lists', listRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/movies', movieRoute);
+app.use('/api/lists', listRoute);
 
 const PORT = process.env.PORT;
 

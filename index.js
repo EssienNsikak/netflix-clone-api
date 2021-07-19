8cbd67ef145a01b6efa8f466cc8a0456fc7dee70
EssienNsikak <nsikak.essienceo@gmail.com>
2021-07-19 15:08:51 +0100

    index.js modified

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
@@ -8,6 +9,19 @@ const movieRoute = require('./routes/movies');
 const listRoute = require('./routes/lists');
 
 dotenv.config();
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  return next();
+});
 
 const MONGO_URL = process.env.MONGO_URL
 mongoose
